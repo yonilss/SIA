f27bb0106382fcd639a0bf2999dd8e2c18dc45d2
Radgelwashere <142951831+SamanoRajelJohann@users.noreply.github.com>
2024-10-12 00:07:41 +0800

    Update data.sql

--- data.sql
+++ data.sql
@@ -5,6 +5,17 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
 DROP TABLE IF EXISTS `employee`;
 CREATE TABLE `employee` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -14,14 +25,9 @@ CREATE TABLE `employee` (
   `Age` int(11) NOT NULL,
   `E-mail` varchar(255) NOT NULL,
   `Address` text NOT NULL,
+  `Image` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_general_ci;
 
-INSERT INTO `employee` (`id`, `Employee_ID`, `First_Name`, `Last_Name`, `Age`, `E-mail`, `Address`) VALUES
-(2,	102,	'Jane',	'Smith',	35,	'jane.smith@example.com',	'5678 Oak St, Metropolis, IL, USA'),
-(3,	103,	'Michael',	'Brown',	42,	'michael.brown@example.com',	'9101 Pine St, Gotham, NY, USA'),
-(4,	104,	'Emily',	'Clark',	30,	'emily.clark@example.com',	'1213 Maple St, Star City, CA, USA'),
-(5,	105,	'Daniel',	'Johnson',	25,	'daniel.johnson@example.com',	'1415 Cedar St, Central City, TX, USA'),
-(6,	1231,	'Earl',	'Panisa',	43,	'butakalgasapo@gmail.com',	'lacson');
 
--- 2024-09-15 13:10:19
+-- 2024-10-11 16:04:08
